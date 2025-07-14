30c677acb576103d23fb19e27ab195cba247e581
Jéssica Pinheiro <jessicasilva.js1314@gmail.com.br>
2025-07-14 15:56:48 -0300

    Corrigindo o processo de exclusão do arquivo selecionado

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -18,18 +18,35 @@ export default function FileUploader({ onFileAccepted }: { onFileAccepted: (file
         accept: { 'application/pdf': ['.pdf'] },
         multiple: false,
     });
+
+    const deleteFileSelected = () => {
+        //onFileAccepted(null);
+        setFileSelected(null)
+    }
     return (
         <div className='w-full h-1/5 flex flex-col items-center justify-center border-4 border-gray-100 rounded-lg p-6'>
-            <div 
+            <div
                 {...getRootProps()}
                 className="w-full h-full flex flex-col items-center justify-center border-3 border-dashed border-gray-200 rounded-lg p-6 text-center cursor-pointer hover:bg-gray-50 transition"
             >
-            <input {...getInputProps()} />
-            <div className='w-full flex flex-row justify-between items-center bg-white px-6 py-6 border-2 border-gray-100 rounded-lg text-left'>
-                <p>{fileSelected ? fileSelected.name : isDragActive ? 'Solte o arquivo aqui...': 'Arraste um PDF ou clique para selecionar'} </p>
-                <Trash className='text-gray-800'/>
+                <input {...getInputProps()} />
+                {
+                    !fileSelected && (
+                        <p>{isDragActive ? 'Solte o arquivo aqui...' : 'Arraste um PDF ou clique para selecionar'} </p>
+                    )
+                }
+                {
+                    fileSelected && (
+                        <div className='w-full flex flex-row justify-between items-center bg-white px-6 py-6 border-2 border-gray-100 rounded-lg text-left'>
+                            <p>{fileSelected ? fileSelected.name : isDragActive ? 'Solte o arquivo aqui...' : 'Arraste um PDF ou clique para selecionar'} </p>
+                            <Trash className='text-gray-800' onClick={(e) => {
+                                e.stopPropagation();
+                                deleteFileSelected();
+                            }} />
+                        </div>
+                    )
+                }
             </div>
-        </div>
         </div>
     )
 }
